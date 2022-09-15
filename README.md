3c18ab0c5b0cf0a0fdfe7db203e4fc6ab69bbc07
boris <borisbrutas@gmail.com>
2022-09-15 20:51:48 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Metacrafters Challenge 2
---
+++ index.js
@@ -0,0 +1,12 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    SystemProgram,
+    sendAndConfirmRawTransaction,
+    sendAndConfirmTransaction
+} = require("@solana/web3.js");
+
